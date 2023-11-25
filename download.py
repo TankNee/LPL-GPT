dce05abaea3cb0020d87ce22e5d3628f4eefdfdc
TankNee <nee@tanknee.cn>
2023-11-25 21:17:57 +0800

    优化重连

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 import argparse
+import functools
 import json
 import os
 import re
@@ -22,6 +23,25 @@ def create_logger():
     )
 
 
+def retry(retry_times, default=None):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for _ in range(retry_times):
+                try:
+                    output = func(*args, **kwargs)
+                    return output if output is not None else default
+                except Exception as e:
+                    logger.warning(f"Function execution failed, retrying... {e}")
+                    time.sleep(30)
+
+            raise Exception("Function execution failed after multiple retries.")
+
+        return wrapper
+
+    return decorator
+
+
 def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--video_list", type=str, default="video_list.json")
@@ -30,11 +50,12 @@ def get_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+@retry(retry_times=5)
 def download(video_url: str, file_name: str, output_dir: str):
     you_get_path = shutil.which("you-get")
     if you_get_path is None:
         raise FileNotFoundError("you-get not found.")
-    
+
     command = f"{you_get_path} --format=dash-flv480 --no-caption --output-filename {file_name} --output-dir {output_dir} {video_url}"
     subprocess.run(command, shell=True, check=True)
 
@@ -49,25 +70,24 @@ def extract_teams_and_date(text):
         team2 = match[2]
         # date变成日期对象
         date = date.replace("月", "-").replace("日", "")
-        date = datetime.strptime("2023-"+date, "%Y-%m-%d").date()
+        date = datetime.strptime("2023-" + date, "%Y-%m-%d").date()
         # date变成20230415这样的数字
         date = date.strftime("%Y%m%d")
         return [team1, team2], date
     else:
         return None, None
 
+
 def has_match(string: str) -> bool:
     pattern = r"第[一二三四五六七八九十\d]+局"
     match = re.search(pattern, string)
     return bool(match)
 
-def get_video_info(video_url: str):
-    # 设置Chrome浏览器驱动的路径
-    driver_path = '/path/to/chromedriver'
 
+def get_video_info(video_url: str):
     # 创建Chrome浏览器选项
     chrome_options = Options()
-    chrome_options.add_argument('--headless')  # 无界面模式
+    chrome_options.add_argument("--headless")  # 无界面模式
 
     # 创建Chrome浏览器驱动
     driver = webdriver.Chrome(options=chrome_options)
@@ -84,7 +104,7 @@ def get_video_info(video_url: str):
     teams, date = extract_teams_and_date(title)
     if not teams:
         logger.info(f"视频{title}不是比赛视频，URL为{video_url}")
-        return None, None, None
+        return None, None, None, None
 
     # 获取播放列表长度
     playlist_path = "#multi_page > div.cur-list > ul"
@@ -94,12 +114,19 @@ def get_video_info(video_url: str):
     for idx, li in enumerate(playlist.find_elements(By.TAG_NAME, "li")):
         text = li.text
         if has_match(text):
-            playlist_idx.append(idx+1)
+            playlist_idx.append(idx + 1)
 
+    icon_list_selector = "#bilibili-player > div > div > div.bpx-player-primary-area > div.bpx-player-video-area > div.bpx-player-control-wrap > div.bpx-player-control-entity > div.bpx-player-control-top > div > div.bpx-player-progress-freezone"
+    icon_list = driver.find_element(By.CSS_SELECTOR, icon_list_selector)
+    # 查找icon_list中第一个子元素的img标签的data-seek属性
+    icon_list = icon_list.find_element(By.TAG_NAME, "img")
+    start_point = icon_list.get_attribute("data-seek")
+    start_point = int(start_point)
     # 关闭浏览器
     driver.quit()
 
-    return teams, date, playlist_idx
+    return teams, date, playlist_idx, start_point
+
 
 def main():
     create_logger()
@@ -117,10 +144,12 @@ def main():
             if any([video_url == record["url"] for record in records]):
                 logger.info(f"视频{video_url}已经下载过了，跳过")
                 continue
-            teams, date, playlist_idx = get_video_info(video_url)
+            teams, date, playlist_idx, start_point = get_video_info(video_url)
             if teams is not None:
-                logger.info(f"获取到了{teams[0]} vs {teams[1]} {date} {len(playlist_idx)}P的比赛视频")
-                
+                logger.info(
+                    f"获取到了{teams[0]} vs {teams[1]} {date} {len(playlist_idx)}P的比赛视频"
+                )
+
                 file_name = f"{teams[0]}_{teams[1]}_{date}"
                 for idx, i in enumerate(playlist_idx):
                     if os.path.exists(f"{args.output_dir}/{file_name}_{idx + 1}.mp4"):
@@ -128,16 +157,31 @@ def main():
                         continue
                     sub_file_name = f"{file_name}_{idx + 1}"
                     logger.info(f"开始下载{sub_file_name}")
-                    download(video_url+f"?p={i}", sub_file_name, args.output_dir)
+                    download(video_url + f"?p={i}", sub_file_name, args.output_dir)
                     time.sleep(15)
-            records.append({
-                "teams": teams,
-                "date": date,
-                "playlist_idx": playlist_idx,
-                "url": video_url,
-            })
-            f.write(json.dumps({"url": video_url, "teams": teams, "date": date, "playlist_idx": playlist_idx})+"\n")
+            records.append(
+                {
+                    "teams": teams,
+                    "date": date,
+                    "playlist_idx": playlist_idx,
+                    "url": video_url,
+                    "start_point": start_point,
+                }
+            )
+            f.write(
+                json.dumps(
+                    {
+                        "url": video_url,
+                        "teams": teams,
+                        "date": date,
+                        "playlist_idx": playlist_idx,
+                        "start_point": start_point,
+                    }
+                )
+                + "\n"
+            )
             f.flush()
+
 
 if __name__ == "__main__":
     main()
