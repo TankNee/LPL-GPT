7ea5c0e3513bf7740180b7e0e9da8fbbcd9a44e0
TankNee <nee@tanknee.cn>
2023-11-25 15:06:22 +0800

    优化下载逻辑，避免重复下载

--- download.py
+++ download.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import os
 import re
+import shutil
 import subprocess
 from datetime import datetime
 import time
@@ -29,7 +30,11 @@ def get_args() -> argparse.Namespace:
 
 
 def download(video_url: str, file_name: str, output_dir: str):
-    command = f"/Users/tanknee/miniforge3/envs/llm/bin/you-get --format=dash-flv480 --no-caption --output-filename {file_name} --output-dir {output_dir} {video_url}"
+    you_get_path = shutil.which("you-get")
+    if you_get_path is None:
+        raise FileNotFoundError("you-get not found.")
+    
+    command = f"{you_get_path} --format=dash-flv480 --no-caption --output-filename {file_name} --output-dir {output_dir} {video_url}"
     subprocess.run(command, shell=True, check=True)
 
 
@@ -103,21 +108,35 @@ def main():
     with open(args.video_list, "r") as f:
         video_list = json.load(f)
 
-    for video_url in video_list:
-        teams, date, playlist_idx = get_video_info(video_url)
-        if teams is None:
-            continue
-        logger.info(f"获取到了{teams[0]} vs {teams[1]} {date} {len(playlist_idx)}P的比赛视频")
-        
-        file_name = f"{teams[0]}_{teams[1]}_{date}"
-        for idx, i in enumerate(playlist_idx):
-            if os.path.exists(f"{args.output_dir}/{file_name}_{idx + 1}.mp4"):
-                logger.info(f"视频{file_name}_{idx + 1}已经存在，跳过")
+    with open("./config/records.jsonl", "a+") as f:
+        f.seek(0)
+        records = [json.loads(line) for line in f]
+        f.seek(0, 2)
+        for video_url in video_list:
+            if any([video_url == record["url"] for record in records]):
+                logger.info(f"视频{video_url}已经下载过了，跳过")
                 continue
-            sub_file_name = f"{file_name}_{idx + 1}"
-            logger.info(f"开始下载{sub_file_name}")
-            download(video_url+f"?p={i}", sub_file_name, args.output_dir)
-            time.sleep(15)
+            teams, date, playlist_idx = get_video_info(video_url)
+            if teams is not None:
+                logger.info(f"获取到了{teams[0]} vs {teams[1]} {date} {len(playlist_idx)}P的比赛视频")
+                
+                file_name = f"{teams[0]}_{teams[1]}_{date}"
+                for idx, i in enumerate(playlist_idx):
+                    if os.path.exists(f"{args.output_dir}/{file_name}_{idx + 1}.mp4"):
+                        logger.info(f"视频{file_name}_{idx + 1}已经存在，跳过")
+                        continue
+                    sub_file_name = f"{file_name}_{idx + 1}"
+                    logger.info(f"开始下载{sub_file_name}")
+                    download(video_url+f"?p={i}", sub_file_name, args.output_dir)
+                    time.sleep(15)
+            records.append({
+                "teams": teams,
+                "date": date,
+                "playlist_idx": playlist_idx,
+                "url": video_url,
+            })
+            f.write(json.dumps({"url": video_url, "teams": teams, "date": date, "playlist_idx": playlist_idx})+"\n")
+            f.flush()
 
 if __name__ == "__main__":
     main()
