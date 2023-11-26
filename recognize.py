f5b8466f2e5618da11526c3ed9a76371213e1adb
TankNee <nee@tanknee.cn>
2023-11-26 21:01:38 +0800

    添加识别脚本

---
+++ recognize.py
@@ -0,0 +1,34 @@
+import argparse
+import glob
+import os
+from loguru import logger
+import whisper
+from whisper import Whisper
+
+from common import create_logger
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--video_dir", type=str, default="videos")
+    parser.add_argument("--output_dir", type=str, default="output")
+    parser.add_argument("--model_path", type=str, default="/path/to/model.pt")
+    return parser.parse_args()
+    
+def recognize(model: Whisper, video_path: str):
+    result = model.transcribe(video_path)
+    return result["text"]
+
+def main():
+    args = get_args()
+    create_logger()
+
+    video_list = glob.glob(os.path.join(args.video_dir, "*.mp4"))
+    # load whisper model
+    model = whisper.load_model(name=args.model_path).cuda()
+    for video_path in video_list:
+        res = recognize(model, video_path)
+        logger.info(f"{video_path}: {res}")
+
+if __name__ == "__main__":
+    main()
