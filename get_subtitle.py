5a7a1054de518de5040b23a6d699450f8a66647a
TankNee <nee@tanknee.cn>
2023-11-27 21:05:14 +0800

    优化识别代码，调整代码格式

--- get_subtitle.py
+++ get_subtitle.py
@@ -2,15 +2,15 @@
 # author = 'ZZH'
 # time = 2023/11/27
 # project = get_subtitle
+import argparse
 import json
 import os
-import time
-from dotenv import load_dotenv
-import jsonlines
+
 import requests
-import argparse
+from dotenv import load_dotenv
 from loguru import logger
-from common import create_logger
+
+from common import create_logger, read_jsonl
 
 load_dotenv()
 
@@ -23,32 +23,35 @@ def get_args() -> argparse.Namespace:
 
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    "Cookie": os.getenv("COOKIE")
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Cookie": os.getenv("COOKIE"),
 }
 CID_VID_URL = "http://api.bilibili.com/x/web-interface/view?bvid="
-
 SUBTITLE_URL = "https://api.bilibili.com/x/player/v2?cid=&s&aid=%s&bvid=%s"
 
 
 def get_cid_vid(bvid: str) -> (str, list):
-    '''
+    """
     :param bvid:
     :return: aid 和 （cid_list 代表每一场信息）
-    '''
+    """
     response = requests.get(CID_VID_URL + bvid, headers=headers)
     text = json.loads(response.text)
-    aid = text['data']['aid']
-    cid_list = [d['cid'] for d in text['data']['pages']]
+    aid = text["data"]["aid"]
+    cid_list = [d["cid"] for d in text["data"]["pages"]]
     return aid, cid_list
 
 
 def get_subtitle(cid, aid, bvid) -> dict:
-    response = requests.get("https://api.bilibili.com/x/player/v2?cid=%s&aid=%s&bvid=%s" % (cid, aid, bvid),
-                            headers=headers)
+    response = requests.get(
+        "https://api.bilibili.com/x/player/v2?cid=%s&aid=%s&bvid=%s" % (cid, aid, bvid),
+        headers=headers,
+    )
     text = json.loads(response.text)
     try:
-        subtitle_url = "https:" + text["data"]['subtitle']['subtitles'][0]['subtitle_url']
+        subtitle_url = (
+            "https:" + text["data"]["subtitle"]["subtitles"][0]["subtitle_url"]
+        )
     except KeyError:
         logger.error(f"糟糕，subtitle_url没有，对应bvid:{bvid},cid:{cid},aid:{aid}")
         subtitle_url = None
@@ -58,23 +61,25 @@ def get_subtitle(cid, aid, bvid) -> dict:
 
 
 def main(args):
-    with open(args.records, 'r') as f:
-        for d in jsonlines.Reader(f):
-            bvid = d['url'].split('/')[-2]
-            if d["playlist_idx"]:
-                playlist_idx = [i - 1 for i in d["playlist_idx"]]  # 下标从0开始
-                base_file_name = os.path.join(args.output_dir, '_'.join(d['teams']) + '_' + d['date'])
-                aid, cid_list = get_cid_vid(bvid)
-                for i, idx in enumerate(playlist_idx):
-                    file_name = base_file_name + '_' + str(i + 1) + '.json'
-                    subtitle_dict = get_subtitle(cid_list[idx], aid, bvid)
-                    with open(file_name, 'w') as f:
-                        logger.info(f"正在往{file_name} 写入字幕数据")
-                        json.dump(subtitle_dict, f, ensure_ascii=False)
+    records = read_jsonl(args.records)
+    for d in records:
+        bvid = d["url"].split("/")[-2]
+        if d["playlist_idx"]:
+            playlist_idx = [i - 1 for i in d["playlist_idx"]]  # 下标从0开始
+            base_file_name = os.path.join(
+                args.output_dir, "_".join(d["teams"]) + "_" + d["date"]
+            )
+            aid, cid_list = get_cid_vid(bvid)
+            for i, idx in enumerate(playlist_idx):
+                file_name = base_file_name + "_" + str(i + 1) + ".json"
+                subtitle_dict = get_subtitle(cid_list[idx], aid, bvid)
+                with open(file_name, "w") as f:
+                    logger.info(f"正在往{file_name} 写入字幕数据")
+                    json.dump(subtitle_dict, f, ensure_ascii=False)
 
 
-if __name__ == '__main__':
-    create_logger(log_file='logs/get_subtitle.log')
+if __name__ == "__main__":
+    create_logger(log_file="logs/get_subtitle.log")
     args = get_args()
     logger.debug(args)
     if not os.path.exists(args.output_dir):
--- recognize.py
+++ recognize.py
@@ -1,9 +1,12 @@
 import argparse
 import glob
 import os
-from loguru import logger
+
+import pandas as pd
 import whisper
-from whisper import Whisper
+from faster_whisper import WhisperModel
+from loguru import logger
+from tqdm import tqdm
 
 from common import create_logger
 
@@ -11,25 +14,50 @@ from common import create_logger
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--video_dir", type=str, default="videos")
-    parser.add_argument("--output_dir", type=str, default="output")
-    parser.add_argument("--model_name", type=str, default="base")
-    parser.add_argument("--model_path", type=str, default="path/to/model")
+    parser.add_argument("--output_dir", type=str, default="outputs")
+    parser.add_argument("--model_name", type=str, default="large")
+    parser.add_argument("--model_path", type=str, default="faster-whisper-large-v2")
     return parser.parse_args()
-    
-def recognize(model: Whisper, video_path: str):
-    result = model.transcribe(video_path)
-    return result["text"]
+
+
+def recognize(model: WhisperModel, video_path: str):
+    df_list = []
+    segments, _ = model.transcribe(
+        video_path,
+        beam_size=5,
+        language="zh",
+        repetition_penalty=1.1,
+        compression_ratio_threshold=2.3,
+        vad_filter=True,
+        vad_parameters=dict(min_silence_duration_ms=800),
+    )
+    logger.info("Transcript the video %s " % video_path)
+    pbar = tqdm(segments)
+    for segment in pbar:
+        df_list.append([segment.start, segment.end, segment.text])
+        pbar.set_postfix(
+            {"End Time": f"{int(segment.end // 60)}:{int(segment.end % 60):02d}"}
+        )
+    df = pd.DataFrame(df_list, columns=["start", "end", "text"])
+    return df
+
 
 def main():
     args = get_args()
-    create_logger()
-
+    create_logger("./logs/recognize.log")
     video_list = glob.glob(os.path.join(args.video_dir, "*.mp4"))
+    model = WhisperModel(
+        model_size_or_path=args.model_path, device="cuda", compute_type="float16"
+    )
     # load whisper model
-    model = whisper.load_model(name=args.model_name, download_root=args.model_path)
-    for video_path in video_list:
-        res = recognize(model, video_path)
-        logger.info(f"{video_path}: {res}")
+    for video_path in tqdm(video_list):
+        segments_df = recognize(model, video_path)
+        csv_path = os.path.join(
+            args.output_dir, os.path.basename(video_path).replace(".mp4", ".csv")
+        )
+        logger.info("Save the result to %s" % csv_path)
+        segments_df.to_csv(csv_path, index=False)
+
 
 if __name__ == "__main__":
     main()
